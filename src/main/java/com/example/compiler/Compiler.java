13ee33f194e28c472b64e16ae3e108dab8d92e63
Veaceslav Grec <grec.vs@gmail.com>
2022-12-25 22:04:46 +0100

    Example with src

--- src/main/java/com/example/compiler/Compiler.java
+++ src/main/java/com/example/compiler/Compiler.java
@@ -3,15 +3,9 @@ package com.example.compiler;
 import com.apollographql.apollo3.compiler.ApolloCompiler;
 import com.apollographql.apollo3.compiler.Options;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Set;
-import java.util.stream.Stream;
 
 public class Compiler {
 
@@ -33,37 +27,5 @@ public class Compiler {
         );
 
         ApolloCompiler.INSTANCE.write(options);
-
-        savePaths(outputDir);
-
-    }
-
-    private static void savePaths(File outputDir) throws IOException {
-        File pathsFile = new File(outputDir, "paths.txt");
-        FileWriter fileWriter = new FileWriter(pathsFile, true);
-        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-
-        String pathPrefix = "src/main/java/com/example/";
-
-        try (Stream<Path> stream = Files.walk(Paths.get(outputDir.getAbsolutePath()))) {
-            stream.filter(path -> path.toString().endsWith(".kt"))
-                    .forEach(path -> {
-                        try {
-                            String filePath = path.toString().substring(
-                                    path.toString().indexOf(pathPrefix) + pathPrefix.length()
-                            );
-                            String line = "ctx.actions.declare_file(\"" + filePath + "\"),";
-                            bufferedWriter.write(line);
-                            bufferedWriter.newLine();
-                        } catch (IOException e) {
-                            throw new RuntimeException(e);
-                        }
-                    });
-        }
-
-        bufferedWriter.close();
-
-        System.out.println("Paths saved at: " + pathsFile.getAbsolutePath());
-
     }
 }
